b0b597ad6c5e1e72d712717bccf88ac567c3c5e4
yvonne <60806722+yyh28@users.noreply.github.com>
2023-04-25 15:00:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = "对不起，因为ChatGPT调用收费，您的免费使用额度已用完~";
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "sk-*****",
 });
 
 const openai = new OpenAIApi(configuration);
